e2023a8766a3e9eeeaf045a4ebeb3783c49c28b1
matias mazzia <matiasmazzia123@gmail.com>
2022-06-03 16:41:42 -0300

    primer commit
    Co-authored-by: Felipe Tau <Feli-dev@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {Route} from 'react-router-dom'
+import Nav from "./components/Nav/Nav"
+import Home from './views/home/Home'
+import Mapamundi from './views/mapamundi/Mapamundi'
+import Feed from "./views/feed/Feed"
+import Form from './views/form/Form'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <Nav/>
+      <Route exact path="/">
+        <Home/>
+      </Route>
+      <Route exact path="/map">
+        <Mapamundi/>
+      </Route>
+      <Route exact path="/trip/:id">
+        <Feed/>
+      </Route>
+      <Route exact path="/addtrip">
+        <Form/>
+      </Route>
+    </div>     
   );
 }
 
