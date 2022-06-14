462ccdcae5e13449b0cd5eda621f138c558b3b64
Felipe Tau <felipetau0@gmail.com>
2022-06-14 19:03:32 -0300

    commiteado

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,12 +2,12 @@ import { Container, Navbar } from "react-bootstrap";
 
 const Nav = () => {
     return (
-        <Navbar bg="dark" variant="dark">
+        <Navbar bg="light" variant="light">
             <Container>
                 <Navbar.Brand href="#home">
                     <img
                     alt=""
-                    src="https://hieloandino.cl/wp-content/uploads/2019/09/whatsapp-logo-png-hd-2.png"
+                    src="https://i.pinimg.com/originals/4a/a5/34/4aa534605d1afc465c5bdc5dd80f1ecb.png"
                     width="30"
                     height="30"
                     className="d-inline-block align-top"
