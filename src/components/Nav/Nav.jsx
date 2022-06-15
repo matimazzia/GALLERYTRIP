a022e9fe61c9303cc8e8d37849aaf527b939fdb8
Felipe Tau <felipetau0@gmail.com>
2022-06-14 21:40:56 -0300

    Changes on Form and Nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,15 +1,25 @@
 import {
-    Editable,
-    EditableInput,
-    EditablePreview,
+    Flex, 
+    Spacer,
+    Box,
+    Heading,
+    ButtonGroup,
+    Button,
+    Center
 } from "@chakra-ui/react";
 
 const Nav = () => {
     return (
-        <Editable defaultValue='Take some chakra'>
-            <EditablePreview />
-            <EditableInput />
-        </Editable>
+        <Flex gap='2' marginTop="1rem" justify="center">
+            <Box p='2'>
+                <Heading size='md'>Chakra App</Heading>
+            </Box>
+            <Spacer></Spacer>
+            <ButtonGroup gap='2'>
+                <Button colorScheme='teal'>Sign Up</Button>
+                <Button colorScheme='teal'>Log in</Button>
+            </ButtonGroup>
+        </Flex>
     )
 }
 
